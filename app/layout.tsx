6b99d2c9853ad0933217e76efd8a7fbddd906f58
Sinned016 <97sinned@gmail.com>
2024-08-29 17:46:26 +0200

    Deleted some useless imports, gonna work on the styling next but all functionality around adding, editing, deleteing and checking is done.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Image from "next/image";
 
-const inter = Inter({ subsets: ["latin"] });
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
